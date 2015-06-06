bfc7f6c0dc26841859f08ecf5a5455f1b6522bcf
Aaron Milam <armilam@armilam-macbook.local>
2015-06-06 13:22:35 -0400

    Added admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:new, :create]
   resources :students
+
+  namespace :admin do
+    resources :dashboard, only: [:index]
+  end
 end
