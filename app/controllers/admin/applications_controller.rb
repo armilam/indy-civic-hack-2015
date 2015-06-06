0234629b7ca13bba2a687134be9cfad65c655d37
Aaron Milam <aaron@lesson.ly>
2015-06-06 17:52:33 -0400

    Application search and style improvements

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -8,5 +8,12 @@ class Admin::ApplicationsController < Admin::ApplicationController
   end
 
   def search
+    @applications = Application
+      .joins(:student)
+      .where("(students.first_name || ' ' || students.middle_name || ' ' || students.last_name) ilike :student_name", student_name: "%#{params[:student_name]}%")
+
+    @student_name = params[:student_name]
+
+    render :index
   end
 end
