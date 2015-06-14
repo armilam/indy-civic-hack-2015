91087be70d4fba111a88bccdbff723bb26c56af4
theAustinSeven <austin@lesson.ly>
2015-06-13 23:44:24 -0400

    image problem

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module RailsDevise
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.assets.js_compressor = :uglifier
     config.assets.enabled = true
 
     # Compress JavaScripts and CSS
